42f914eaae26e9f9c03bfa4cb4612ec92470475a
Anand Kumria <akumria@acm.org>
2012-05-10 09:27:39 +0100

    Make the config files slightly easier to read.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,12 @@
 class nullmailer::config {
   file { '/etc/nullmailer/remotes':
-    content => "$nullmailer::remoterelay smtp",
+    content => "$nullmailer::remoterelay smtp\n",
     require => Class['nullmailer::package'],
     notify  => Class['nullmailer::service'],
   }
 
   file { '/etc/nullmailer/adminaddr':
-    content => "$nullmailer::adminaddr",
+    content => "$nullmailer::adminaddr\n",
     require => Class['nullmailer::package'],
     notify  => Class['nullmailer::service'],
   }
